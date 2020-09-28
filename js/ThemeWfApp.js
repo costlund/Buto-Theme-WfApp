ab67326d3f5111fcc25801a40ff122d2a51e36a6
Claes Ö <>
2020-09-28 21:39:13 +0200

    1.1.1

--- js/ThemeWfApp.js
+++ js/ThemeWfApp.js
@@ -1,9 +1,9 @@
 function ThemeWfApp(){
   this.signin = function(btn){
-    PluginWfBootstrapjs.modal({id: 'modal_signin', url: '/account/signin', label: btn.innerHTML});
+    PluginWfBootstrapjs.modal({id: 'modal_signin', url: '/account/signin', label: btn.innerHTML, size: 'sm'});
   }
   this.create = function(btn){
-    PluginWfBootstrapjs.modal({id: 'modal_create', url: '/account/create', label: btn.innerHTML});
+    PluginWfBootstrapjs.modal({id: 'modal_create', url: '/account/create', label: btn.innerHTML, size: 'sm'});
   }
   this.on_signin = function(){
     alert('on_signin...');
